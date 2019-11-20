ac9301ea222d8d63b77064eab2142bac739e9a56
Egor Shkirya <zuhijan@yandex.ru>
2019-11-20 19:03:51 +0600

    centr error

---
+++ middlewares/errors.js
@@ -0,0 +1,12 @@
+module.exports = (err, req, res) => {
+  const { statusCode = 500, message } = err;
+
+  res
+    .status(statusCode)
+    .send({
+      // проверяем статус и выставляем сообщение в зависимости от него
+      message: statusCode === 500
+        ? 'На сервере произошла ошибка'
+        : message,
+    });
+};
